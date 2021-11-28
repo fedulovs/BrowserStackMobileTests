b26a5c4230f13401035cdad4fca298ca4467fdd6
s.fedulov <s.fedulov@corp.vk.com>
2021-11-28 16:17:20 +0300

    Initial commit

---
+++ src/test/java/drivers/BrowserstackMobileDriver.java
@@ -0,0 +1,50 @@
+package drivers;
+
+import com.codeborne.selenide.WebDriverProvider;
+import config.CredentialsConfig;
+import io.appium.java_client.android.AndroidDriver;
+import org.aeonbits.owner.ConfigFactory;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BrowserstackMobileDriver implements WebDriverProvider {
+
+    public static CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
+
+    String login = config.login();
+    String pass = config.pass();
+
+    public static URL getBrowserstackUrl() {
+        try {
+            return new URL("http://hub.browserstack.com/wd/hub");
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public WebDriver createDriver(DesiredCapabilities desiredCapabilities) {
+
+        // Set your access credentials
+        desiredCapabilities.setCapability("browserstack.user", "heaven_TeYz4a");
+        desiredCapabilities.setCapability("browserstack.key", "ypaQu2sDo1TXbUdS9mb5");
+
+        // Set URL of the application under test
+        desiredCapabilities.setCapability("app", "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c");
+
+        // Specify device and os_version for testing
+        desiredCapabilities.setCapability("device", "Google Pixel 3");
+        desiredCapabilities.setCapability("os_version", "9.0");
+
+        // Set other BrowserStack capabilities
+        desiredCapabilities.setCapability("project", "First Java Project");
+        desiredCapabilities.setCapability("build", "Java Android");
+        desiredCapabilities.setCapability("name", "first_test");
+
+        return new AndroidDriver(getBrowserstackUrl(), desiredCapabilities);
+    }
+}
+
