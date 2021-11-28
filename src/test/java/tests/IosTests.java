90a7b3fe84f0cf7d76a669c2627a60e68590f55d
s.fedulov <s.fedulov@corp.vk.com>
2021-11-28 16:29:10 +0300

    Disable iOS test

--- src/test/java/tests/IosTests.java
+++ src/test/java/tests/IosTests.java
@@ -14,6 +14,7 @@ import java.net.URL;
 
 public class IosTests {
 
+    @Disabled
     @Test
     void searchTest() throws MalformedURLException, InterruptedException {
         DesiredCapabilities caps = new DesiredCapabilities();
