aa4d011a141323e0fdbfd1ea6742c1aafc911652
s.fedulov <s.fedulov@corp.vk.com>
2021-11-28 17:12:07 +0300

    Change config variables

--- src/test/java/helpers/Browserstack.java
+++ src/test/java/helpers/Browserstack.java
@@ -7,11 +7,15 @@ import static io.restassured.RestAssured.given;
 
 public class Browserstack {
 
-    public static CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
-
     public static String videoUrl(String sessionId) {
+
+        CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
+
+        String login = config.login();
+        String pass = config.pass();
+
         return given()
-                .auth().basic(config.login(), config.pass())
+                .auth().basic(login, pass)
                 .when()
                 .get("https://api-cloud.browserstack.com/app-automate/sessions/" + sessionId +".json")
                 .then()
